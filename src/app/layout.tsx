dd42f1c8aea0267c798fb4af2937d8ecf78bd8e6
dimasmuhamad2017-maker <dimasmuhamad2017@gmail.com>
2025-12-19 19:56:26 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="5s372exzhparpt9oj7hlktkhnclpkt" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
